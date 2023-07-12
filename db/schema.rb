3a9ce684f43d2a3ab9f77f6dd79f184a20d63d8c
Vadim Voichenko <vvojchenko0@gmail.com>
2023-07-12 19:33:06 +0300

    Configured validation

---
+++ db/schema.rb
@@ -0,0 +1,49 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_134240) do
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.float "engine_capacity"
+    t.string "fuel_type"
+    t.float "daily_price"
+    t.float "fuel_consumption"
+    t.string "img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.integer "car_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "date_from"
+    t.datetime "date_to"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_rentals_on_car_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
+    t.date "birth_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "rentals", "cars"
+  add_foreign_key "rentals", "users"
+end
