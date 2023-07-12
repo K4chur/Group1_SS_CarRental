a75891baeec486049b55f096d9c818ef384b1d93
Vadim Voichenko <vvojchenko0@gmail.com>
2023-07-12 17:25:05 +0300

    Configured validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,12 @@
 class Car < ApplicationRecord
   has_many :rentals
+
+  validates :brand, presence: true, length: { maximum: 255 }
+  validates :model, presence: true, length: { maximum: 255 }
+  validates :engine_capacity, presence: true, numericality: { greater_than: 0 }
+  validates :fuel_type, presence: true, length: { maximum: 255 }
+  validates :daily_price, presence: true, numericality: { greater_than: 0 }
+  validates :fuel_consumption, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :img, presence: true
+
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,18 @@
 class Rental < ApplicationRecord
   belongs_to :car
   belongs_to :user
+
+  validates :car, presence: true
+  validates :user, presence: true
+  validates :date_from, presence: true
+  validates :date_to, presence: true
+
+  validate :date_to_after_date_from
+
+  private
+  def date_to_after_date_from
+    return if date_to.blank? || date_from.blank?
+    errors.add(:date_to, "must be after date_from") if date_to <= date_from
+  end
+
 end
