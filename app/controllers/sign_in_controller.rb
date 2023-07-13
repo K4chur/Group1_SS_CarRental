fd0adb64ae5a277ba59480f4f7ab344bf21d5700
Dmitry-Dem <demenchuk.dmitry03@gmail.com>
2023-07-13 22:19:49 +0300

    Created Sign Up and Sing In pages, also controllers for them

---
+++ app/controllers/sign_in_controller.rb
@@ -0,0 +1,5 @@
+class SignInController < ApplicationController
+    def index
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "sign_up", to: "sign_up#index"
+
+  root "sign_in#index"
 end
