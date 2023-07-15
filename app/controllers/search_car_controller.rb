f8b028c16d797ca1c07bd008ce9829e4c8b8968f
Dmitry-Dem <demenchuk.dmitry03@gmail.com>
2023-07-15 15:54:01 +0300

    Added home and search_car pages

---
+++ app/controllers/search_car_controller.rb
@@ -0,0 +1,5 @@
+class SearchCarController < ApplicationController
+    def index
+
+    end
+end
--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -1,5 +1,28 @@
 class SignUpController < ApplicationController 
-    def index
-        
+    layout "base_layout"
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        respond_to do |format|
+          if @user.save
+            session[:user_id] = @user.id
+
+            format.html { redirect_to home_path, notice: "User was successfully created." }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    private 
+    
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :birth_date, :email, :password)
     end
 end
