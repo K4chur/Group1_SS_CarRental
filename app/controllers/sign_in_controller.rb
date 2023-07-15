5ebbfb56096c89a0ce106c728f22d1ea16ee5139
Dmitry-Dem <demenchuk.dmitry03@gmail.com>
2023-07-15 15:54:01 +0300

    Added home and search_car pages

--- app/controllers/sign_in_controller.rb
+++ app/controllers/sign_in_controller.rb
@@ -1,5 +1,16 @@
 class SignInController < ApplicationController
+    layout "base_layout"
+    
     def index
-        
+        @user = User.new
+    end
+
+    def login
+        #test code
+        if !session[:user_id]
+            session[:user_id] = User.first.id
+        end
+
+        redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,22 @@ Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "sign_up", to: "sign_up#index"
+  root "home#index"
 
-  root "sign_in#index"
+  # Sign_in Page
+  get "sign_in", to: "sign_in#index"
+  post "sign_in", to: "sign_in#login"
+
+  # Sign_up Page
+  get "sign_up", to: "sign_up#new"
+  post "sign_up", to: "sign_up#create"
+
+  # Home Page
+  get "logout", to: "home#logout"
+
+  # Search_car Page
+  get "search_car", to: "search_car#index"
+
+  # Profile Page
+  get "profile", to: "profile#index"
 end
